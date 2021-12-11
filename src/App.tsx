01a0f53192ab39e899d7aafe351c4aa1b3b83b8e
kapureka6174 <mitsu.n.0109@gmail.com>
2021-12-12 08:26:29 +0900

    [add] レスポンシブ用のCSSを追加

--- src/App.tsx
+++ src/App.tsx
@@ -53,40 +53,52 @@ const App = () => {
     }
   };
 
-  return (<div className='h-screen'>
-    <div className="flex flex-col items-center p-4 my-12 mx-auto w-1/2 h-1/2 bg-gray-100 rounded-lg border-4">
-      <h1 className="text-4xl text-blue-400">クイズアプリ</h1>
-      {gameOver && (
-        <button className="my-32 w-2/3 h-12 text-3xl text-white bg-blue-600 rounded-md" onClick={startTrivia}>
-          スタート
-        </button>
-      )}
-      {loading && <p>問題を読み込んでいます...</p>}
-      {!loading && !gameOver && (
-        <QuestionCard
-          score={score}
-          questionNr={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
-          callback={checkAnswer}
-        />
-      )}
-      {!gameOver &&
-      !loading &&
-      userAnswers.length === number + 1 &&
-      number !== TOTAL_QUESTIONS - 1 ? (
-        <button className='py-1 px-6 my-2 text-white bg-gray-500 hover:bg-gray-700 rounded-md' onClick={() => setNumber(prev  => prev+1)}>
-          次の問題へ
-        </button>
-      ) : null}
-      {userAnswers[number]?.question === questions[number]?.question && number === TOTAL_QUESTIONS - 1 && !gameOver && (
-        <button className='py-1 px-6 my-2 text-white bg-gray-500 hover:bg-gray-700 rounded-md' onClick={() => setGameOver(true)}>
-          スタート画面へ
-        </button>
-      )}
-    </div>
+  return (
+    <div className="h-screen">
+      <div className="flex flex-col items-center p-4 my-12 mx-auto md:w-1/2 h-3/5 md:h-1/2 bg-gray-100 rounded-lg border-4">
+        <h1 className="text-4xl text-blue-400">クイズアプリ</h1>
+        {gameOver && (
+          <button
+            className="my-32 w-2/3 h-12 text-3xl text-white bg-blue-600 rounded-md"
+            onClick={startTrivia}
+          >
+            スタート
+          </button>
+        )}
+        {loading && <p>問題を読み込んでいます...</p>}
+        {!loading && !gameOver && (
+          <QuestionCard
+            score={score}
+            questionNr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver &&
+        !loading &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button
+            className="py-1 px-6 my-2 text-white bg-gray-500 hover:bg-gray-700 rounded-md"
+            onClick={() => setNumber((prev) => prev + 1)}
+          >
+            次の問題へ
+          </button>
+        ) : null}
+        {userAnswers[number]?.question === questions[number]?.question &&
+          number === TOTAL_QUESTIONS - 1 &&
+          !gameOver && (
+            <button
+              className="py-1 px-6 my-2 text-white bg-gray-500 hover:bg-gray-700 rounded-md"
+              onClick={() => setGameOver(true)}
+            >
+              スタート画面へ
+            </button>
+          )}
+      </div>
     </div>
   );
 };
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import type { AnswerObject } from '../App';
 
 type Props = {
-  score: number
+  score: number;
   question: string;
   answers: string[];
   callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -22,21 +22,35 @@ const QuestionCard: React.FC<Props> = ({
   questionNr,
   totalQuestions,
 }) => (
-  <div className='w-full'>
-    <div className='flex flex-col items-center'>
-      <div className='flex my-1'>
-        <p className='mr-32'>スコア：{score}</p>
+  <div className="w-full">
+    <div className="flex flex-col items-center">
+      <div className="flex my-1">
+        <p className="mr-32">スコア：{score}</p>
         <p>
           問題： {questionNr} / {totalQuestions}
         </p>
       </div>
-      <p className='my-1 h-12'>{question}</p>
+      <p className="my-8">{question}</p>
     </div>
     <div>
       {answers.map((answer) => (
         <div key={answer} className="flex flex-col items-center">
-          <button className="p-2 my-1 w-4/5 bg-blue-100 hover:bg-blue-300 rounded-md" disabled={!!userAnswer} value={answer} onClick={callback}>
-            <span className={(answer === userAnswer?.correctAnswer ? 'text-green-500' : '') + (userAnswer && answer !== userAnswer?.correctAnswer ? 'text-red-500': '' )}>{answer}</span>
+          <button
+            className="p-2 my-1 w-4/5 bg-blue-100 hover:bg-blue-300 rounded-md"
+            disabled={!!userAnswer}
+            value={answer}
+            onClick={callback}
+          >
+            <span
+              className={
+                (answer === userAnswer?.correctAnswer ? 'text-green-500' : '') +
+                (userAnswer && answer !== userAnswer?.correctAnswer
+                  ? 'text-red-500'
+                  : '')
+              }
+            >
+              {answer}
+            </span>
           </button>
         </div>
       ))}
