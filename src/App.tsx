f9378f2b466ed218c9062415795134e04f61c7e9
kapureka6174 <mitsu.n.0109@gmail.com>
2021-12-11 22:12:21 +0900

    [add] スタート、次の問題、回答チェック機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,95 @@
-const App = () => <div>クイズアプリ</div>;
+import { useState } from 'react';
+import { fetchQuizQuestions, QuestionState, Difficulty } from './API';
 
+// Components
+import QuestionCard from './components/QuestionCard';
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
+      if (correct) setScore((prev) => prev + 1);
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    const nextNumber = number + 1;
+
+    if (nextNumber === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextNumber);
+    }
+  };
+
+  return (
+    <div>
+      <h1>クイズアプリ</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          スタート
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">スコア：{score}</p> : null}
+      {loading && <p>問題を読み込んでいます...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          次の問題へ
+        </button>
+      ) : null}
+    </div>
+  );
+};
 export default App;
